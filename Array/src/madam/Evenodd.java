a686199dc23359a78c6c153e3bf32e0673ba5936
Admin <Admin@DESKTOP-D0KCSOU>
2023-06-02 17:36:01 +0530

    NEW PROGRAM

---
+++ Array/src/madam/Evenodd.java
@@ -0,0 +1,14 @@
+package madam;
+
+public class Evenodd {
+public static void main(String[] args) {
+	int num =100;
+	if (num%2==0) {
+		System.out.println("EVEN");
+		
+	} else {
+		System.out.println("ODD");
+
+	}
+}
+}
