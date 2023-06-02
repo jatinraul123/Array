41b97db59c94861fb2224d44ddd3c3fb471e51d6
Admin <Admin@DESKTOP-D0KCSOU>
2023-06-02 17:43:57 +0530

    even

--- Array/src/madam/Evenodd.java
+++ Array/src/madam/Evenodd.java
@@ -4,10 +4,10 @@ public class Evenodd {
 public static void main(String[] args) {
 	int num =100;
 	if (num%2==0) {
-		System.out.println("EVEN number");
+		System.out.println("EVEN numbers");
 		
 	} else {
-		System.out.println("ODD number");
+		System.out.println("ODD numbers");
 
 	}
 }
