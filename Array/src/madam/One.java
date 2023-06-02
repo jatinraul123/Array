e5cb6e398d1dbe3366cb2ee3824ca81257f8acd3
Admin <Admin@DESKTOP-D0KCSOU>
2023-06-02 12:19:38 +0530

    update 1.0.0

---
+++ Array/src/madam/One.java
@@ -0,0 +1,22 @@
+package madam;
+
+import java.util.Arrays;
+
+public class One {
+	public static void main(String[] args) {
+		int[] arr = new int[5];
+		int[] arr1 = {1, 2, 3, 4, 5 };
+	System.out.println(Arrays.toString(arr));
+	System.out.println(Arrays.toString(arr1));
+	System.out.println(arr1[4]);
+	arr1[3]=0;
+	System.out.println(Arrays.toString(arr1));
+	arr1[3]=arr1[4];
+	arr1[4]=0;
+	System.out.println(Arrays.toString(arr1));
+	
+
+
+	}
+
+}
