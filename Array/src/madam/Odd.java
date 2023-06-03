c787c280b58312158396bb2a9212fe3170c609cc
Admin <Admin@DESKTOP-D0KCSOU>
2023-06-03 10:49:52 +0530

    oddone

---
+++ Array/src/madam/Odd.java
@@ -0,0 +1,15 @@
+package madam;
+
+public class Odd {
+	public static void main(String[] args) {
+		int num = 131;
+		if (num % 2 == 0) {
+			System.out.println("even");
+
+		} else {
+			System.out.println("odd");
+
+		}
+	}
+
+}
