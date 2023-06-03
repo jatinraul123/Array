a075bfdf4dae0bf36ffeb4ff4997448af84db6bf
Admin <Admin@DESKTOP-D0KCSOU>
2023-06-03 10:53:18 +0530

    odd2

--- Array/src/madam/Odd.java
+++ Array/src/madam/Odd.java
@@ -4,10 +4,10 @@ public class Odd {
 	public static void main(String[] args) {
 		int num = 131;
 		if (num % 2 == 0) {
-			System.out.println("even");
+			System.out.println("even number");
 
 		} else {
-			System.out.println("odd");
+			System.out.println("odd number");
 
 		}
 	}
